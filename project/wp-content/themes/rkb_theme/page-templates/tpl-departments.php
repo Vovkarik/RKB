42328806a96ab3e422a9758b2684da4adeff9e3b
nastyaaa19980 <nastyaaa19980@gmail.com>
2020-03-31 19:56:41 +0300

    Страница отделений

--- project/wp-content/themes/rkb_theme/page-templates/tpl-departments.php
+++ project/wp-content/themes/rkb_theme/page-templates/tpl-departments.php
@@ -10,50 +10,50 @@
         <div class="container">
             <div class="row">
                 <div class="col-4">
-                    <h2 class="b-history__h1">Хирургия</h2>
+                    <h2 class="b-main__h1">Хирургия</h2>
                     <ul class="b-department__list">
-                        <li class="b-department__item"><a href="">Травматология</a></li>
-                        <li class="b-department__item"><a href="">Гинекология</a></li>
-                        <li class="b-department__item"><a href="">Гнойная хирургия</a></li>
-                        <li class="b-department__item"><a href="">ЛОР</a></li>
-                        <li class="b-department__item"><a href="">Нейрохирургия</a></li>
-                        <li class="b-department__item"><a href="">Операционное отделение</a></li>
-                        <li class="b-department__item"><a href="">Реанимация №1</a></li>
-                        <li class="b-department__item"><a href="">Реанимация №2</a></li>
-                        <li class="b-department__item"><a href="">Торокально-сосудистая хирургия</a></li>
-                        <li class="b-department__item"><a href="">Урология</a></li>
-                        <li class="b-department__item"><a href="">Хирургия</a></li>
-                        <li class="b-department__item"><a href="">ЧЛХ</a></li>
+                        <li class="b-department__item"><a href="/traumatology">Травматология</a></li>
+                        <li class="b-department__item"><a href="/gynecology">Гинекология</a></li>
+                        <li class="b-department__item"><a href="/purulent-surgery">Гнойная хирургия</a></li>
+                        <li class="b-department__item"><a href="/otorhinolaryngology">ЛОР</a></li>
+                        <li class="b-department__item"><a href="/neurosurgery">Нейрохирургия</a></li>
+                        <li class="b-department__item"><a href="/operating-room">Операционное отделение</a></li>
+                        <li class="b-department__item"><a href="/resuscitation-1">Реанимация №1</a></li>
+                        <li class="b-department__item"><a href="/resuscitation-2">Реанимация №2</a></li>
+                        <li class="b-department__item"><a href="/thoracic-vascular-surgery">Торокально-сосудистая хирургия</a></li>
+                        <li class="b-department__item"><a href="/urology">Урология</a></li>
+                        <li class="b-department__item"><a href="/surgery">Хирургия</a></li>
+                        <li class="b-department__item"><a href="/maxillofacial-surgery">ЧЛХ</a></li>
                     </ul>
                 </div>
                 <div class="col-4">
-                    <h2 class="b-history__h1">Диагностика</h2>
+                    <h2 class="b-main__h1">Диагностика</h2>
                     <ul class="b-department__list">
-                        <li class="b-department__item"><a href="">Бактериологическая лаборатория</a></li>
-                        <li class="b-department__item"><a href="">Клинико-диагностические лаборатория</a></li>
-                        <li class="b-department__item"><a href="">Патологоанатомическое отделение</a></li>
-                        <li class="b-department__item"><a href="">Приемное отделение</a></li>
-                        <li class="b-department__item"><a href="">Рентгенология</a></li>
-                        <li class="b-department__item"><a href="">Функциональная диагностика</a></li>
-                        <li class="b-department__item"><a href="">Эндоскопия</a></li>
+                        <li class="b-department__item"><a href="/bacteriological-laboratory">Бактериологическая лаборатория</a></li>
+                        <li class="b-department__item"><a href="/clinical-diagnostic-laboratory">Клинико-диагностические лаборатория</a></li>
+                        <li class="b-department__item"><a href="/pathology-department">Патологоанатомическое отделение</a></li>
+                        <li class="b-department__item"><a href="/reception-office">Приемное отделение</a></li>
+                        <li class="b-department__item"><a href="/radiology">Рентгенология</a></li>
+                        <li class="b-department__item"><a href="/functional-diagnostics">Функциональная диагностика</a></li>
+                        <li class="b-department__item"><a href="/endoscopy">Эндоскопия</a></li>
+                        <li class="b-department__item b-department__item_special"><a href="/clinic">Поликлиника</a></li>
                     </ul>
                 </div>
                 <div class="col-4">
-                    <h2 class="b-history__h1">Терапия</h2>
+                    <h2 class="b-main__h1">Терапия</h2>
                     <ul class="b-department__list">
-                        <li class="b-department__item"><a href="">Гастроэнтерология</a></li>
-                        <li class="b-department__item"><a href="">Гемодиализ</a></li>
-                        <li class="b-department__item"><a href="">Кардиология</a></li>
-                        <li class="b-department__item"><a href="">Неврология</a></li>
-                        <li class="b-department__item"><a href="">Нефрология</a></li>
-                        <li class="b-department__item"><a href="">Отделение восстановительного лечения</a></li>
-                        <li class="b-department__item"><a href="">Пульмонология</a></li>
-                        <li class="b-department__item"><a href="">Ревматология</a></li>
-                        <li class="b-department__item"><a href="">Эндокринология</a></li>
+                        <li class="b-department__item"><a href="/gastroenterology">Гастроэнтерология</a></li>
+                        <li class="b-department__item"><a href="/hemodialysis">Гемодиализ</a></li>
+                        <li class="b-department__item"><a href="/cardiology">Кардиология</a></li>
+                        <li class="b-department__item"><a href="/neurology">Неврология</a></li>
+                        <li class="b-department__item"><a href="/nephrology">Нефрология</a></li>
+                        <li class="b-department__item"><a href="/rehabilitation-department">Отделение восстановительного лечения</a></li>
+                        <li class="b-department__item"><a href="/pulmonology">Пульмонология</a></li>
+                        <li class="b-department__item"><a href="/rheumatology">Ревматология</a></li>
+                        <li class="b-department__item"><a href="/endocrinology">Эндокринология</a></li>
                     </ul>
                 </div>
                 <div class="col-4">
-                    <h2 class="b-history__h1 b-department-margin">Поликлиника</h2>
                 </div>
             </div>
         </div>
