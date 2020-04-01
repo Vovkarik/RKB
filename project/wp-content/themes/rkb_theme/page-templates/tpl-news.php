ee03b6996a69b1e251151b953f689d6b25296b93
trapeznikova-evgenija <trapeznikova.evg@yandex.ru>
2020-04-01 16:46:20 +0300

    добавила страницу новостей

---
+++ project/wp-content/themes/rkb_theme/page-templates/tpl-news.php
@@ -0,0 +1,35 @@
+<?php
+/**
+ * Template Name: News
+ */
+
+    get_header();
+
+    $current_page = (get_query_var('paged')) ? get_query_var('paged') : 1; // определяем текущую страницу блога
+    $args = array(
+        'posts_per_page' => get_option('posts_per_page'), // значение по умолчанию берётся из настроек, но вы можете использовать и собственное
+        'paged'          => $current_page // текущая страница
+    );
+    query_posts( $args );
+
+    $wp_query->is_archive = true;
+    $wp_query->is_home = false;
+
+    $counter = 1;
+?>
+
+<div class="b-news-container">
+    <div class="container">
+        <h1 class="b-title__h2 b-news__title-margin">Новости</h1>
+        <?php while(have_posts()): the_post(); ?>
+            <div class="b-news__item<? if ($counter%2 !== 0): ?> b-news__item-blue<? endif;?>">
+                <h3 class="b-news__title"><?php the_title() ?></h3>
+                <p class="b-news__text"><?php the_content() ?></p>
+            </div>
+            <?php $counter++ ?>
+        <?php endwhile; ?>
+    </div>
+</div>
+
+<?php
+get_footer();
